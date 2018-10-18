414a73eb16aa77c7e28189cb5591170676a9c185
Rodrigo Papadopulo <rpapadopulo.2106@gmail.com>
2018-10-18 11:08:57 -0300

    Commit nuevo

--- webapp/src/app/app-routing/routes.ts
+++ webapp/src/app/app-routing/routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { MenuComponent } from '../components/menu/menu.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { AboutComponent } from '../components/about/about.component';
+import { DishdetailComponent } from '../components/dishdetail/dishdetail.component';
 
 export const routes = [
     { path: 'home', component: HomeComponent},
     { path: 'menu', component: MenuComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
+    { path: 'contact', component: ContactComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'dishdetail/:id', component: DishdetailComponent },
+    { path: '**', redirectTo: '/home' }
 ];
